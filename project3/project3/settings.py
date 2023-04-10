493441b61ed9c156461dca015a658c147148f8ea
Лебедев Михаил Семенович <lebedev-ms@gse.globse.local>
2023-04-10 18:01:00 +0800

    added bootstrap template

--- project3/project3/settings.py
+++ project3/project3/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages',
 ]
 
 SITE_ID = 1
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
